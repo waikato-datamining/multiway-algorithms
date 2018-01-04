4d8b984457a22740ccdff444775d741c52f59c51
Steven Lang <steven.lang.mz@gmail.com>
2018-01-04 17:01:24 +1300

    Add random data generation for tests

--- src/test/java/com/github/waikatodatamining/multiway/TestUtils.java
+++ src/test/java/com/github/waikatodatamining/multiway/TestUtils.java
@@ -24,4 +24,25 @@ public class TestUtils {
     field.setAccessible(true);
     return (T) field.get(obj);
   }
+
+  /**
+   * Generate a random 3d data tensor
+   *
+   * @param dim1 First dimension
+   * @param dim2 Second dimension
+   * @param dim3 Third dimension
+   * @return 3D Data tensor
+   */
+  public static double[][][] generateRandomTensor(int dim1, int dim2, int dim3) {
+    double[][][] data= new double[dim1][dim2][dim3];
+    for (int i = 0; i < dim1; i++) {
+      for (int j = 0; j < dim2; j++) {
+        for (int k = 0; k < dim3; k++) {
+          data[i][j][k] = Math.random();
+        }
+      }
+    }
+//    INDArray data = Nd4j.randn(new int[]{dim1, dim2, dim3}, 0);
+    return data;
+  }
 }
