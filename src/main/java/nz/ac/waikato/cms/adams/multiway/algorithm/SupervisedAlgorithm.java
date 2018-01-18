332c14f1a087ebe4a2957ccbdcd2ef87277295bf
Steven Lang <steven.lang.mz@gmail.com>
2018-01-18 19:13:52 +1300

    alg: Differentiate between un/supervised algorithms

---
+++ src/main/java/nz/ac/waikato/cms/adams/multiway/algorithm/SupervisedAlgorithm.java
@@ -0,0 +1,51 @@
+package nz.ac.waikato.cms.adams.multiway.algorithm;
+
+import nz.ac.waikato.cms.adams.multiway.data.tensor.Tensor;
+
+/**
+ * Abstract class for supervised algorithms. Similar to
+ * {@link UnsupervisedAlgorithm}, while all defined methods accept an additional
+ * {@link Tensor} with dependent variables.
+ *
+ * @author Steven Lang
+ */
+public abstract class SupervisedAlgorithm extends AbstractAlgorithm {
+
+  private static final long serialVersionUID = 6756079034435363940L;
+
+  /**
+   * Check the input and return an error message if something went wrong, else
+   * null.
+   *
+   * @param x Data tensor
+   * @param y Learning target
+   * @return Error message if error, else null
+   */
+  protected abstract String check(Tensor x, Tensor y);
+
+  /**
+   * Run the actual build. Return error message if something went wrong, else
+   * null.
+   *
+   * @param x Data tensor
+   * @param y Learning target
+   * @return Error message if error, else null
+   */
+  protected abstract String doBuild(Tensor x, Tensor y);
+
+  /**
+   * Public build method for the user. Return error message if something went
+   * wrong, else null.
+   *
+   * @param x Data tensor
+   * @param y Learning target
+   * @return Error message if error, else null
+   */
+  public final String build(Tensor x, Tensor y) {
+    String result = check(x, y);
+    if (result == null)
+      result = doBuild(x, y);
+    return result;
+  }
+
+}
---
+++ src/main/java/nz/ac/waikato/cms/adams/multiway/algorithm/UnsupervisedAlgorithm.java
@@ -0,0 +1,41 @@
+package nz.ac.waikato.cms.adams.multiway.algorithm;
+
+import nz.ac.waikato.cms.adams.multiway.data.tensor.Tensor;
+
+public abstract class UnsupervisedAlgorithm extends AbstractAlgorithm {
+
+  private static final long serialVersionUID = 6756079034435363940L;
+
+  /**
+   * Check the input and return an error message if something went wrong, else
+   * null.
+   *
+   * @param x Data tensor
+   * @return Error message if error, else null
+   */
+  protected abstract String check(Tensor x);
+
+  /**
+   * Run the actual build. Return error message if something went wrong, else
+   * null.
+   *
+   * @param x Data tensor
+   * @return Error message if error, else null
+   */
+  protected abstract String doBuild(Tensor x);
+
+  /**
+   * Public build method for the user. Return error message if something went
+   * wrong, else null.
+   *
+   * @param x Data tensor
+   * @return Error message if error, else null
+   */
+  public final String build(Tensor x) {
+    String result = check(x);
+    if (result == null)
+      result = doBuild(x);
+    return result;
+  }
+
+}
