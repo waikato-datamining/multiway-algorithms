8376ff7d6457d41bb255ab252e59f44168f2f602
Steven Lang <steven.lang.mz@gmail.com>
2018-09-13 16:49:52 +0200

    tests: Add supervised regression test manager

--- src/test/java/nz/ac/waikato/cms/adams/multiway/algorithm/regression/SupervisedRegressionTestManager.java
+++ src/test/java/nz/ac/waikato/cms/adams/multiway/algorithm/regression/SupervisedRegressionTestManager.java
@@ -1,11 +1,32 @@
 package nz.ac.waikato.cms.adams.multiway.algorithm.regression;
 
+import nz.ac.waikato.cms.adams.multiway.TestUtils;
 import nz.ac.waikato.cms.adams.multiway.algorithm.api.SupervisedAlgorithm;
+import nz.ac.waikato.cms.adams.multiway.data.tensor.Tensor;
+
+import java.io.IOException;
 
 public abstract class SupervisedRegressionTestManager<E extends SupervisedAlgorithm, R> extends RegressionTestManager<E, R> {
 
   @Override
-  public final boolean run() {
-    return false;
+  public final boolean run() throws IOException {
+    Tensor[] data = getRegressionTestData();
+    algorithm.build(data[0], data[1]);
+    if (checkIfReferenceExists()) {
+      return resultEqualsReference();
+    }
+    else {
+      saveNewReference();
+      return true;
+    }
   }
+
+  public Tensor[] getRegressionTestData() {
+    return TestUtils.loadSyntheticSupervisedData();
+  }
+  @Override
+  public String getRegressionReferenceDirectory() {
+    return getRegressionReferenceBaseDirectory() + "/supervised/synthetic/ref";
+  }
+
 }
